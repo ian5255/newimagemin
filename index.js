2d17fe25ff3556ebb41cf7064d7a29386536af7b
ian5258 <ian@axolotl.com.tw>
2021-11-09 16:24:16 +0800

    add worder_thread

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const sharp = require('sharp');
 const fs = require('fs');
 const convert = require('heic-convert');
 const {promisify} = require('util');
+const { Worker } = require('worker_threads');
+// const { exit } = require('process');
+// const worker = new Worker('./worker.js');
 
 async function readFileHandle(sourcePath) {
   return fs.readdirSync(sourcePath);
@@ -69,6 +72,7 @@ async function rotateImage() {
 
 // 圖片壓縮處理程序
 async function ImageCompressHandler(sourcePath, destinationPath, quality) {
+  console.log("開始進行壓縮工作");
   const start = new Date().getTime();
   const files = await readFileHandle(sourcePath);
   if (files === []) {
@@ -112,4 +116,77 @@ async function ImageCompressHandler(sourcePath, destinationPath, quality) {
   console.log("總耗時："+(end - start) / 1000 + "sec");
 }
 
-ImageCompressHandler('src/assets/origin', 'src/assets/result', 30);
+// ImageCompressHandler('src/assets/origin', 'src/assets/result', 30);
+
+
+// worker_threads ====================================================================================================
+const runService = (sourcePath, destinationPath, quality, files) => {
+  if (files.length === 0) return;
+  return new Promise((resolve, reject) => {
+    let countSuccessNum = 0; // 成功數
+    let countFailedNum = 0; // 失敗數
+    let countHandleNum = 0; // 處理檔案數
+    let countTimeConsuming = 0; // 總耗時
+    let workerNum = 5; // 開幾條線程
+    const fileNumAvg = parseInt(files.length / workerNum);
+    for (let i = 0; i < workerNum; i++) {
+      let workerFileArr = [];
+      if (i === (workerNum - 1)) {
+        workerFileArr = files.slice((i * fileNumAvg), (((i + 1) * fileNumAvg) + fileNumAvg % workerNum)); // 切分分派的filesArr
+      } else {
+        workerFileArr = files.slice((i * fileNumAvg), ((i + 1) * fileNumAvg)); // 切分分派的filesArr
+      }
+      
+      const worker = new Worker('./worker.js');
+      worker.postMessage({
+        fileArr: workerFileArr,
+        start: i, // 起始
+        end: (((i + 1) * fileNumAvg) - 1), // 結束
+        sourcePath: sourcePath,
+        destinationPath: destinationPath,
+        quality: quality
+      });
+
+      // 接收worker handle result
+      worker.on('message', countObj => {
+        countSuccessNum += countObj.successed; // 成功數
+        countFailedNum += countObj.failed; // 失敗數
+        countHandleNum += countObj.total; // 處理檔案數
+        countTimeConsuming += countObj.timeConsuming; // 總耗時
+        if (files.length === countHandleNum) {
+          console.info("\n總耗時：" + countTimeConsuming + " sec。處理檔案總數：" + countHandleNum + "。成功：" + countSuccessNum + "。失敗：" + countFailedNum);
+        }
+      });
+      worker.on('error', err => {
+        console.info(err);
+      });
+      worker.on('exit', (code) => {
+        console.log("code：", code);
+          if (code !== 0)
+              reject(new Error(`stopped with  ${code} exit code`));
+      });
+    }
+  });
+}
+
+const run = async () => {
+  console.log("開始進行壓縮工作");
+  // step1 get sourcePath files
+  const files = await readFileHandle('src/assets/origin');
+  if (files === []) {
+    console.log('目錄無檔案！');
+    return;
+  }
+
+  // step2 開線程分派工作
+  const result = await runService('src/assets/origin', 'src/assets/result', 70, files);
+
+  console.log(result);
+}
+run().catch(err => console.error(err));
+
+// worker.postMessage('send to worker');
+// // 監聽: 接收worker回報的資料
+// worker.on('message', msg => {
+//     console.info(msg);
+// });
